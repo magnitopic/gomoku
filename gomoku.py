bdb9a98101d7553217a252a98f2b6e882dbca610
magnitopic <magnitopic@gmail.com>
2025-03-19 21:11:03 +0100

    border

--- gomoku.py
+++ gomoku.py
@@ -4,15 +4,16 @@ import pygame
 pygame.init()
 
 # Constants
-BOARD_SIZE = 855
-COLS, ROWS = 19, 19
-MARGIN = 20
-
-CELL_SIZE = (BOARD_SIZE - MARGIN) // COLS
+BOARD_SIZE = 920
+COLS, ROWS = 18, 18
+MARGIN = 35
+BORDER_MARGIN = 30
+CELL_SIZE = (BOARD_SIZE - 2 * MARGIN) // COLS
 
 # Colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+BORDER_COLOR = (150, 150, 150)
 
 # Create the screen
 screen = pygame.display.set_mode((BOARD_SIZE, BOARD_SIZE))
@@ -20,13 +21,20 @@ pygame.display.set_caption("gomoku")
 
 
 def draw_board():
+    border_rect = pygame.Rect(
+        MARGIN - BORDER_MARGIN,
+        MARGIN - BORDER_MARGIN,
+        COLS * CELL_SIZE + 2 * BORDER_MARGIN,
+        ROWS * CELL_SIZE + 2 * BORDER_MARGIN
+    )
+    pygame.draw.rect(screen, BORDER_COLOR, border_rect, 2)
+
     for i in range(ROWS + 1):
         pygame.draw.line(
             screen, WHITE,
             (MARGIN, MARGIN + i * CELL_SIZE),
-
             (MARGIN + COLS * CELL_SIZE, MARGIN + i * CELL_SIZE),
-            1
+            2
         )
 
     for i in range(COLS + 1):
@@ -34,7 +42,7 @@ def draw_board():
             screen, WHITE,
             (MARGIN + i * CELL_SIZE, MARGIN),
             (MARGIN + i * CELL_SIZE, MARGIN + ROWS * CELL_SIZE),
-            1
+            2
         )
 
     pygame.display.flip()
