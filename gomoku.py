aed7540d29e221dada6d24268af4629ead115051
magnitopic <magnitopic@gmail.com>
2025-03-19 12:39:17 +0100

    drawing board 19x19

--- gomoku.py
+++ gomoku.py
@@ -4,15 +4,40 @@ import pygame
 pygame.init()
 
 # Constants
-WIDTH, HEIGHT = 800, 600
+BOARD_SIZE = 855
+COLS, ROWS = 19, 19
+MARGIN = 20
+
+CELL_SIZE = (BOARD_SIZE - MARGIN) // COLS
 
 # Colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 # Create the screen
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((BOARD_SIZE, BOARD_SIZE))
 pygame.display.set_caption("gomoku")
+
+
+def draw_board():
+    for i in range(ROWS + 1):
+        pygame.draw.line(
+            screen, WHITE,
+            (MARGIN, MARGIN + i * CELL_SIZE),
+
+            (MARGIN + COLS * CELL_SIZE, MARGIN + i * CELL_SIZE),
+            1
+        )
+
+    for i in range(COLS + 1):
+        pygame.draw.line(
+            screen, WHITE,
+            (MARGIN + i * CELL_SIZE, MARGIN),
+            (MARGIN + i * CELL_SIZE, MARGIN + ROWS * CELL_SIZE),
+            1
+        )
+
+    pygame.display.flip()
 
 
 def main():
@@ -30,6 +55,7 @@ def main():
 
         # Drawing
         screen.fill(BLACK)
+        draw_board()
 
         clock.tick(10)
 
